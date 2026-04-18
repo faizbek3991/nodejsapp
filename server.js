d105301e91e493fb3b6eeb9fffde3b5e8bdc1e0f
Cikgu Faiz <faiz.bek3991@gmail.com>
2026-04-18 11:54:58 +0800

    Initial commit with CI/CD workflow

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const http = require('http');
+require('dotenv').config();
+
+const app = express();
+const server = http.createServer(app);
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, MERN Full stack development batch 3 2026!');
+});
+
+app.get('/api/data', (req, res) => {
+  res.json({ message: 'This is some data from the server.' });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+}); 
